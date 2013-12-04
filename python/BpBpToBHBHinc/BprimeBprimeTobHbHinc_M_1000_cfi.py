943107676e9a87006810d6990be707ae8bad7af3
dmajumder <devdatta.majumder@cern.ch>
2013-12-04 10:15:33 -0600

    Inout cfi for LPC

---
+++ python/BpBpToBHBHinc/BprimeBprimeTobHbHinc_M_1000_cfi.py
@@ -0,0 +1,12 @@
+FileNames = [ 
+'dcache:/pnfs/cms/WAX/11/store/user/devdatta/NtuplesBprimeTobH_v1/BprimeBprimeToBHBHinc_M-1000_TuneZ2star_8TeV-madgraph/BprimeTobH_v1_10_1_Xgr.root',
+'dcache:/pnfs/cms/WAX/11/store/user/devdatta/NtuplesBprimeTobH_v1/BprimeBprimeToBHBHinc_M-1000_TuneZ2star_8TeV-madgraph/BprimeTobH_v1_1_1_zu3.root',
+'dcache:/pnfs/cms/WAX/11/store/user/devdatta/NtuplesBprimeTobH_v1/BprimeBprimeToBHBHinc_M-1000_TuneZ2star_8TeV-madgraph/BprimeTobH_v1_2_1_MLQ.root',
+'dcache:/pnfs/cms/WAX/11/store/user/devdatta/NtuplesBprimeTobH_v1/BprimeBprimeToBHBHinc_M-1000_TuneZ2star_8TeV-madgraph/BprimeTobH_v1_3_1_0K3.root',
+'dcache:/pnfs/cms/WAX/11/store/user/devdatta/NtuplesBprimeTobH_v1/BprimeBprimeToBHBHinc_M-1000_TuneZ2star_8TeV-madgraph/BprimeTobH_v1_4_1_n3y.root',
+'dcache:/pnfs/cms/WAX/11/store/user/devdatta/NtuplesBprimeTobH_v1/BprimeBprimeToBHBHinc_M-1000_TuneZ2star_8TeV-madgraph/BprimeTobH_v1_5_1_8aI.root',
+'dcache:/pnfs/cms/WAX/11/store/user/devdatta/NtuplesBprimeTobH_v1/BprimeBprimeToBHBHinc_M-1000_TuneZ2star_8TeV-madgraph/BprimeTobH_v1_6_1_vHy.root',
+'dcache:/pnfs/cms/WAX/11/store/user/devdatta/NtuplesBprimeTobH_v1/BprimeBprimeToBHBHinc_M-1000_TuneZ2star_8TeV-madgraph/BprimeTobH_v1_7_1_nOK.root',
+'dcache:/pnfs/cms/WAX/11/store/user/devdatta/NtuplesBprimeTobH_v1/BprimeBprimeToBHBHinc_M-1000_TuneZ2star_8TeV-madgraph/BprimeTobH_v1_8_1_sF1.root',
+'dcache:/pnfs/cms/WAX/11/store/user/devdatta/NtuplesBprimeTobH_v1/BprimeBprimeToBHBHinc_M-1000_TuneZ2star_8TeV-madgraph/BprimeTobH_v1_9_1_agx.root',
+]
